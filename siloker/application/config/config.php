b1094257cd5c6dc228651ec5212f0b5f4857512a
Muhammad Ismail <72483245+hasyim404@users.noreply.github.com>
2021-06-30 03:33:45 +0700

    Update config.php

--- siloker/application/config/config.php
+++ siloker/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ourproject/siloker/';
+$config['base_url'] = 'http://localhost/project_sem2/siloker -main/';
 
 /*
 |--------------------------------------------------------------------------
