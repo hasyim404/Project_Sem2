a295fbbefdea5d47fe2087cc5c7e879286cc1049
Muhammad Ismail <72483245+hasyim404@users.noreply.github.com>
2021-07-03 20:28:32 +0700

    Add files via upload

--- siloker/application/controllers/admin.php
+++ siloker/application/controllers/admin.php
@@ -5,7 +5,8 @@ class admin extends CI_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->model('admin_model','admin');
-        $this->load->model('isiloker_model', 'mitra');
+        $this->load->model('isiloker_model', 'loker');
+        $this->load->model('daftarmitra_model','form');
 
     }
     public function index(){
@@ -16,75 +17,104 @@ class admin extends CI_Controller {
 
     public function kelola_loker(){
         $data['data_loker']=$this->admin->getlokerAll();
-        $data['data_mitra'] = $this->mitra->getmitraAll();
 
         $this->load->view('layouts/header');
         $this->load->view('admin/kelola_loker',$data);
         $this->load->view('layouts/footer');
     }
 
-        public function submit_loker(){
-            $_deskripsi_pekerjaan   =   $this->input->post('deskripsi_pekerjaan');
-            $_tanggal_akhir         =   $this->input->post('tanggal_akhir');
-            $_mitra_id              =   $this->input->post('mitra_id');
-            $_email                 =   $this->input->post('email');
-
-            $data = array(  'deskripsi_pekerjaan'   =>  $_deskripsi_pekerjaan,
-                            'tanggal_akhir'         =>  $_tanggal_akhir,
-                            'mitra_id'              =>  $_mitra_id,
-                            'email'                 =>  $_email,
-            );
-
-            $this->admin->save($data);
-            redirect(base_url('index.php/admin/kelola_loker'),'refresh');
-        }
-    
-        public function edit($id){
+        public function edit_loker($id){
             $obj_loker = $this->admin->getById($id);
             $data['objloker']=$obj_loker;
-            $data['data_mitra'] = $this->mitra->getmitraAll();
+            $data['data_mitra'] = $this->loker->getmitraAll();
 
             $this->load->view('layouts/header');
-            $this->load->view('admin/edit',$data);
+            $this->load->view('admin/edit_loker',$data);
             $this->load->view('layouts/footer');
         }
-
-        
-        public function update(){
+        public function update_loker(){
             $_deskripsi_pekerjaan   =   $this->input->post('deskripsi_pekerjaan');
             $_tanggal_akhir         =   $this->input->post('tanggal_akhir');
             $_mitra_id              =   $this->input->post('mitra_id');
             $_email                 =   $this->input->post('email');
             $_idedit                =   $this->input->post('idedit');
-
+    
             $data = array(  'deskripsi_pekerjaan'   =>  $_deskripsi_pekerjaan,
                             'tanggal_akhir'         =>  $_tanggal_akhir,
                             'mitra_id'              =>  $_mitra_id,
                             'email'                 =>  $_email,
             );
-
+    
             if (!empty($_idedit)){
                 $data['id']=$_idedit;
-                $this->admin->update($data);
+                $this->loker->update($data);
             }else{
-                $this->admin->save($data);
+                $this->loker->save($data);
             }
-
-            
+    
             redirect(base_url('index.php/admin/kelola_loker'),'refresh');
         }
-
-        public function delete($id){
+    
+        public function delete_loker($id){
             $data['id']=$id;
-            $this->admin->delete($data);
-
+            $this->loker->delete($data);
+    
             redirect(base_url('index.php/admin/kelola_loker'),'refresh');
         }
 
     public function kelola_mitra(){
+        $data['data_mitra']=$this->form->getmitraAll();
+
         $this->load->view('layouts/header');
-        $this->load->view('admin/kelola_mitra');
+        $this->load->view('admin/kelola_mitra',$data);
         $this->load->view('layouts/footer');
+    }
+
+    public function edit_mitra($id){
+        $obj_mitra = $this->form->getmitraById($id);
+        $data['objmitra']=$obj_mitra;
+        $data['data_bdusaha'] = $this->form->getbdusahaAll();
+        $data['data_shusaha'] = $this->form->getshusahaAll();
+
+        $this->load->view('layouts/header');
+        $this->load->view('admin/edit_mitra',$data);
+        $this->load->view('layouts/footer');
+    }
+    public function update_mitra(){
+        $_nama              =   $this->input->post('nama');
+        $_alamat            =   $this->input->post('alamat');
+        $_kontak            =   $this->input->post('kontak');
+        $_telpon            =   $this->input->post('telpon');
+        $_email             =   $this->input->post('email');
+        $_web               =   $this->input->post('web');
+        $_bidang_usaha_id   =   $this->input->post('bidang_usaha_id');
+        $_sektor_usaha_id   =   $this->input->post('sektor_usaha_id');
+
+        $data = array(  'nama'              =>  $_nama,
+                        'alamat'            =>  $_alamat,
+                        'kontak'            =>  $_kontak,
+                        'telpon'            =>  $_telpon,
+                        'email'             =>  $_email,
+                        'web'               =>  $_web,
+                        'bidang_usaha_id'   =>  $_bidang_usaha_id,
+                        'sektor_usaha_id'   =>  $_sektor_usaha_id,
+        );
+
+        if (!empty($_idedit)){
+            $data['id']=$_idedit;
+            $this->form->update($data);
+        }else{
+            $this->form->save($data);
+        }
+
+        redirect(base_url('index.php/admin/kelola_mitra'),'refresh');
+    }
+
+    public function delete_mitra($id){
+        $data['id']=$id;
+        $this->form->delete($data);
+
+        redirect(base_url('index.php/admin/kelola_mitra'),'refresh');
     }
 
     public function isi_berita(){
--- siloker/application/controllers/isi_loker.php
+++ siloker/application/controllers/isi_loker.php
@@ -4,6 +4,7 @@ class isi_loker extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
+        $this->load->model('admin_model', 'admin');
         $this->load->model('isiloker_model', 'model');
     }
 
@@ -14,6 +15,22 @@ class isi_loker extends CI_Controller {
         $this->load->view('isi_loker/index',$data);
         $this->load->view('layouts/footer');
         
+    }
+
+    public function submit_loker(){
+        $_deskripsi_pekerjaan   =   $this->input->post('deskripsi_pekerjaan');
+        $_tanggal_akhir         =   $this->input->post('tanggal_akhir');
+        $_mitra_id              =   $this->input->post('mitra_id');
+        $_email                 =   $this->input->post('email');
+
+        $data = array(  'deskripsi_pekerjaan'   =>  $_deskripsi_pekerjaan,
+                        'tanggal_akhir'         =>  $_tanggal_akhir,
+                        'mitra_id'              =>  $_mitra_id,
+                        'email'                 =>  $_email,
+        );
+
+        $this->model->save($data);
+        redirect(base_url('index.php/isi_loker'),'refresh');
     }
     
 }
