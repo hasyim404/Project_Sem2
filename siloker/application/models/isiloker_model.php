0e3f50b2e6b630a915527ff46dd277cfb2a96d33
Muhammad Ismail <72483245+hasyim404@users.noreply.github.com>
2021-07-03 20:29:22 +0700

    Add files via upload

--- siloker/application/models/isiloker_model.php
+++ siloker/application/models/isiloker_model.php
@@ -11,5 +11,20 @@
             return $query->row();
         }
 
+        public function save($data){
+            $sql = "INSERT INTO lowongan (deskripsi_pekerjaan,tanggal_akhir,mitra_id,email) VALUES (?,?,?,?)";
+            $this->db->query($sql, $data);
+        }
+        
+        public function update($data){
+            $sql =  "UPDATE lowongan SET deskripsi_pekerjaan=?,tanggal_akhir=?,mitra_id=?,email=? WHERE  id=?";
+            $this->db->query($sql, $data);
+        }
+
+        public function delete($data){
+            $sql = "DELETE FROM lowongan WHERE id=?";
+            $this->db->query($sql, $data);
+        }
+
     }
 ?>
